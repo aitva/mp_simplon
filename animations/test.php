c9a86085bdfcfa5cafe3c4147954b7eecd96ab4c
Maelle <maelle.perron.26@gmail.com>
2017-04-27 16:04:15 +0200

    Envoi Git, avec ajour d'un fichier de test ppour php

---
+++ animations/test.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+	<meta charset="utf-8" />
+	<meta name="description" content="Description de la page pour les moteurs de recherche" />
+	<meta name="keywords" content="mots-clefs séparés par des virgules" />
+	<title>TITRE EN ONGLET ET EN RESULTAT GOOGLE</title>
+	<link rel="stylesheet" href="DOSSIER/PAGE.css" />
+	<script type="text/javascript" src="DOSSIER/FICHIER.js"></script>
+	<!--[if lt IE 9]>
+		<script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+	<![endif]--> <!--si t'es un vieux navigateur, t'as besoin de ça pour comprendre la nouvelle ossature de page issue de html5-->
+</head>
+
+<body>
+	<?php echo 'php maaaaaaaaaaaaaaaaaaaarche !' ; ?>
+
+</body>
+</html>
